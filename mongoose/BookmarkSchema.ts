6a75e5d99232274fa8516d7c759f32304df69dcb
xqiao20 <xqiao18@gmail.com>
2022-02-25 17:11:05 -0500

    update doc of bookmark schema

--- mongoose/BookmarkSchema.ts
+++ mongoose/BookmarkSchema.ts
@@ -6,11 +6,12 @@ import mongoose, {Schema} from "mongoose";
 import Bookmark from "../models/Bookmark";
 
 /**
- * @typeof Bookmark Represents bookmarks relationship between a user and a tuit,
+ * @typedef Bookmark Represents bookmarks relationship between a user and a tuit,
  * as in a user bookmarks a tuit
  * @property {ObjectId} bookmarkedTuit Primary key of bookmarked tuit
  * @property {ObjectId} bookmarkedBy Primary key of user bookmarked the tuit
  */
+
 const BookmarkSchema = new mongoose.Schema<Bookmark>({
     bookmarkedTuit : {type: Schema.Types.ObjectId, ref: "TuitModel"},
     bookmarkedBy : {type: Schema.Types.ObjectId, ref: "UserModel"},
